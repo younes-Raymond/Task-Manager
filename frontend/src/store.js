e8e0f8d82eaf980f0fafe771a43baa684cd7e25e
younes1337 <raymondyounes2@gmail.com>
2023-04-23 20:06:35 +0100

    the last production

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,45 +1,45 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// // import { newProductReducer, newReviewReducer, productDetailsReducer, productReducer,  productReviewsReducer,productListReducer } from './reducers/productReducer';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { newProductReducer, newReviewReducer, productDetailsReducer, productReducer,  productReviewsReducer,productListReducer } from './reducers/productReducer';
 
-// const reducer = combineReducers({
-//     productDetails: productDetailsReducer,
-//     products:productListReducer,
-//     newReview: newReviewReducer,
-//     newProduct: newProductReducer,
-//     product: productReducer,
-//     reviews: productReviewsReducer,
-//     review: newReviewReducer,
-// });
+const reducer = combineReducers({
+    productDetails: productDetailsReducer,
+    products:productListReducer,
+    // newReview: newReviewReducer,
+    // newProduct: newProductReducer,
+    // product: productReducer,
+    // reviews: productReviewsReducer,
+    // review: newReviewReducer,
+});
 
-// let initialState = {
-//     cart: {
-//         cartItems: localStorage.getItem('cartItems')
-//             ? JSON.parse(localStorage.getItem('cartItems'))
-//             : [],
-//         shippingInfo: localStorage.getItem("shippingInfo")
-//             ? JSON.parse(localStorage.getItem("shippingInfo"))
-//             : {},
-//     },
-//     saveForLater: {
-//         saveForLaterItems: localStorage.getItem('saveForLaterItems')
-//             ? JSON.parse(localStorage.getItem('saveForLaterItems'))
-//             : [],
-//     },
-//     wishlist: {
-//         wishlistItems: localStorage.getItem('wishlistItems')
-//             ? JSON.parse(localStorage.getItem('wishlistItems'))
-//             : [],
-//     },
-// };
+let initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+        shippingInfo: localStorage.getItem("shippingInfo")
+            ? JSON.parse(localStorage.getItem("shippingInfo"))
+            : {},
+    },
+    saveForLater: {
+        saveForLaterItems: localStorage.getItem('saveForLaterItems')
+            ? JSON.parse(localStorage.getItem('saveForLaterItems'))
+            : [],
+    },
+    wishlist: {
+        wishlistItems: localStorage.getItem('wishlistItems')
+            ? JSON.parse(localStorage.getItem('wishlistItems'))
+            : [],
+    },
+};
 
-// const middleware = [thunk];
+const middleware = [thunk];
 
-// const store = createStore(
-//     reducer,
-//     initialState,
-//     composeWithDevTools(applyMiddleware(...middleware))
-// );
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
 
-// export default store;
+export default store;
