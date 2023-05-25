6dd77b9b024819791e682c3fe97b968dab326c8e
younes1337 <raymondyounes2@gmail.com>
2023-05-26 00:53:55 +0100

    => cannot read property of null error done in login => also we need to make a logic of approved reject req and update localstorage , with with date

--- frontend/src/components/Home/Login/Login.jsx
+++ frontend/src/components/Home/Login/Login.jsx
@@ -64,6 +64,8 @@ function LoginPage() {
     
   
   
+      // const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      // const formattedDate = new Date(user.requestData.takenRequest.requestDate).toLocaleDateString('ar', options);
   
 
 
@@ -81,9 +83,10 @@ function LoginPage() {
   }, [user]);
 
 
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const formattedDate = new Date(user.requestData.takenRequest.requestDate).toLocaleDateString('ar', options);
 
+  // const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  // const formattedDate = new Date(user.requestData.takenRequest.requestDate).toLocaleDateString('ar', options);
+  
   return (
     <div className="login-page">
       <form onSubmit={handleSubmit} className="login-form">
@@ -118,74 +121,64 @@ function LoginPage() {
             New to Allamrt? <Link to="/register">Register here</Link>
           </p>
         </div>
-
-
-
-
-
+  
         {!loading && user && (
-      <div className="chosse-container">
-        <div className="welcome-user">
-          <img src={user.requestData.user.avatar.url} alt="" />
-          <p>
-        Welcome back, 
-        {user.requestData.user.gender === "female" && " Ms "}
-        {user.requestData.user.gender === "male" && " Mc "}
-        {user.requestData.user.name}! What would you like to visit first?
-      </p>
-        </div>
-        {user.requestData.user.role === 'admin' && (
-          <div className="dashboard">
-            <Link to="/add-material">
-              <button>Dashboard</button>
-            </Link>
+          <div className="chosse-container">
+            <div className="welcome-user">
+              <img src={user.requestData.user.avatar.url} alt="" />
+              <p>
+                Welcome back,
+                {user.requestData.user.gender === "female" && " Ms "}
+                {user.requestData.user.gender === "male" && " Mc "}
+                {user.requestData.user.name}! What would you like to visit first?
+              </p>
+            </div>
+            {user.requestData.user.role === 'admin' && (
+              <div className="dashboard">
+                <Link to="/add-material">
+                  <button>Dashboard</button>
+                </Link>
+              </div>
+            )}
+            <div className="materials">
+              <Link to="/show-products">
+                <button>See Materials</button>
+              </Link>
+            </div>
+            <div className="home">
+              <Link to="/">
+                <button>Home</button>
+              </Link>
+            </div>
+            {user.requestData.message && (
+              <p className='hint'>
+                <span>Hint:</span>
+                {user.requestData.message}!
+              </p>
+            )}
+            {user.requestData.takenRequest.materialPicture && user.requestData.takenRequest.requestDate && (
+              <div className='material-info'>
+                <p>
+                  <span>At: {new Date(user.requestData.takenRequest.requestDate).toLocaleDateString('ar', options)}</span>
+                </p>
+                <img src={user.requestData.takenRequest.materialPicture} alt="material picture" />
+              </div>
+            )}
+            {user.requestData.takenRequest && (
+              <div className="approval-buttons">
+                <button onClick={handleApprove} className="approve-button">Approved</button>
+                <button onClick={handleReject} className="reject-button">Rejected</button>
+              </div>
+            )}
           </div>
         )}
-        <div className="materials">
-          <Link to="/show-products">
-            <button>See Materials</button>
-          </Link>
-        </div>
-        <div className="home">
-          <Link to="/">
-            <button>Home</button>
-          </Link>
-        </div>
-        {user.requestData.message && (
-          <p className='hint'>
-            <span>Hint:</span>
-            {user.requestData.message}!
-          </p>
-        )}
-        {/* {user.requestData.} */}
-        {user.requestData.takenRequest.materialPicture && user.requestData.takenRequest.requestDate && (
-  <div className='material-info'>
-    <p>
-      <span>At: {new Date(user.requestData.takenRequest.requestDate).toLocaleDateString('ar', options)}</span>
-    </p>
-    <img src={user.requestData.takenRequest.materialPicture} alt="material picture" />
-  </div>
-)}
-
-        {user.requestData.takenRequest && (
-          <div className="approval-buttons">
-            <button onClick={handleApprove} className="approve-button">Approved</button>
-            <button onClick={handleReject} className="reject-button">Rejected</button>
-          </div>
-        )}
-      </div>
-    )}
-
-
-
-
-
-
-
-        {loading && <p>Loading...</p>} {/* show loading message while waiting for user data */}
+  
+        {loading && <p>Loading...</p>}
       </form>
     </div>
   );
+  
+
 }
 
 export default LoginPage;
