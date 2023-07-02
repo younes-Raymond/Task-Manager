d28e609ae1ecbcec519c798167dd30718ba2de4e
younes1337 <raymondyounes2@gmail.com>
2023-07-02 21:29:32 +0100

    fix the start and endpoint path make the new style add forms and more

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -207,14 +207,13 @@ exports.rejectRequest = asyncErrorHandler(async (req, res) => {
 
 exports.confirmTaken = asyncErrorHandler(async (req, res) => {
     const requestId = req.body.approvedRequests[0].requestId;
-  
+    
     // Remove the document with the specified requestId from the database
     await MaterialRequest.findOneAndRemove({ requestId });
   
     console.log(`Document with requestId ${requestId} removed from the database.`);
-  });
+});
   
-
 exports.search = async (req, res) => {
     console.log(req.query)
     const { keyword } = req.query;
