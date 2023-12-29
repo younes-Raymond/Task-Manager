3d6981db4ab7fa545cb07b806e38a8a4da88a66a
younes1337 <raymondyounes2@gmail.com>
2023-12-29 13:12:39 +0100

    return to last version

--- taskmanagementproject/backend/server.js
+++ taskmanagementproject/backend/server.js
@@ -1,72 +1,269 @@
 const app = require('./app');
-const express = require('express');
-const http = require('http');
+const express = require('express')
 const path = require('path');
-const cors = require('cors');
 const connectDatabase = require('./config/database');
 const cloudinary = require('cloudinary');
-const { v4: uuidv4 } = require('uuid');
-const { Server } = require('socket.io');
-
 const PORT = process.env.PORT || 4000;
+const nodemailer = require('nodemailer');
+const fs =  require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-// Create an HTTP server
-const httpServer = http.createServer(app);
+const {
+  BestWebAgencyInRabat,
+  AU,
+  MATANGER,
+  INTERNATIONLEMAILS,
+  GEBERLINE,
+  GEFrankfurt,
+  SPMADRID,
+  UKLONDONEMAILS,
+  FRPARIS,
+  UKMANCHESTER,
+  emailArray,
+  ListOfCompaniesOfCanada,
+  ListOfMoroccoRecruiter,
+  ListOfNewYorkRecruiter,
+  NewListOfCanada
+ } = require('./Data.js');
 
-// Use CORS middleware
-app.use(cors());
 
-// Create a Socket.IO server attached to the HTTP server
-const io = new Server(httpServer, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
-
-// Handle Socket.IO connection event
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // Handle 'message' event
-  socket.on('message', (data) => {
-    console.log('Received message:', data);
-
-    // Broadcast the message to all connected clients
-    io.emit('message', data);
-  });
-
-  // Handle 'disconnect' event
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
-// Handle uncaughtException Error
+// UncaughtException Error
 process.on('uncaughtException', (err) => {
-  console.log(`Error: ${err.message}`);
-  process.exit(1);
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
 });
 
-// Connect to the database
 connectDatabase();
 
-// Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Start the HTTP server
-httpServer.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
 });
-
+// Unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    });
+});
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Start the Express app
+// Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+
+
+
+const generateUniqueId = () => {
+  // console.log(uuidv4)
+  return uuidv4();
+};
+
+
+
+const sendEmail = async () => {
+
+  // create reusable transporter object using the default SMTP transport
+  let gmailtransporter = nodemailer.createTransport({
+    service: 'gmail',
+    tls: {
+      rejectUnauthorized: false,
+    },
+    auth: {
+      user: 'raymondyounes2@gmail.com',
+      pass: "qxjfzpqyrinhmqza",
+    },
+  });
+
+  const hotmailTransporter = nodemailer.createTransport({
+    host: 'smtp.live.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'your-hotmail-email@hotmail.com',
+      pass: 'your-hotmail-password',
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+
+  const frTransporter = nodemailer.createTransport({
+    host: 'smtp.live.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'your-hotmail-email@hotmail.com',
+      pass: 'your-hotmail-password',
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+
+  });
+
+//const how to creat more and ore shecduke a meettinf 
+// i want the same id who stored in emailLog.json file to send via the api in the img src arttbute 
+
+const testEmailList =  [
+ 'youneshero436@gmail.com'
+]
+  const emailArrays = [
+    testEmailList
+  // BestWebAgencyInRabat,
+  // UKLONDONEMAILS,
+  // NewListOfCanada,
+  // ListOfCompaniesOfCanada,
+  // ListOfMoroccoRecruiter,
+  // ListOfNewYorkRecruiter,
+  // GEBERLINE,
+  // SPMADRID,
+  // emailArray,
+  // AU,
+  // GEFrankfurt,
+  // UKMANCHESTER,
+  // MATANGER,
+  // INTERNATIONLEMAILS,
+  // GEFrankfurt,
+  // FRPARIS,
+  // emailArray,
+  ];
+
+  const batchSize = 50; //400
+  const pauseDuration =   30 * 60 * 1000;  // 25 * 60 * 60 * 1000
+  const emailLog = [];
+  for (const emailArray of emailArrays) {
+    for (let i = 0; i < emailArray.length; i++) {
+      const batch = emailArray.slice(i, i + batchSize);
+
+      for (const companyEmail of batch) {
+        const messageId = generateUniqueId();
+        let transporter;
+        if (companyEmail.endsWith('@gmail.com')||companyEmail.endsWith('.com') ) {
+          transporter = gmailtransporter;
+        } else if (companyEmail.endsWith('@hotmail.com') || companyEmail.endsWith('@outlook.com')) {
+          transporter = hotmailTransporter;
+        } else if (companyEmail.endsWith('.fr')) {
+          transporter = frTransporter;
+        } else {
+          // Handle other email providers if needed
+          console.error(`Unsupported email provider for ${companyEmail}`);
+          continue; // Skip to the next iteration
+        }
+
+        // setup email data
+        let mailOptions = {
+          from: 'raymondyounes2@gmail.com',
+          to: 'youneshero436@gmail.com',
+          subject: 'Job Application',
+          html: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+                body {
+                    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+                    line-height: 1.6;
+                    color: #333;
+                    margin: 0;
+                    padding: 20px;
+                }
+        
+                p {
+                    margin-bottom: 10px;
+                }
+        
+                .button-container {
+                    margin-top: 20px;
+                }
+        
+                .button {
+                    background-color: #2196F3;
+                    border: none;
+                    color: white;
+                    text-align: center;
+                    text-decoration: none;
+                    display: inline-block;
+                    font-size: 16px;
+                    cursor: pointer;
+                    padding: 10px 20px;
+                    border-radius: 4px;
+                }
+            </style>
+            <title>Cover Letter</title>
+        </head>
+        <body>
+            <p>Dear Hiring Manager,</p>
+            <p>I am writing to express my interest in the open position at your company. Please find my cover letter attached for your consideration.</p>
+            <p>Best regards,</p>
+            <a style="text-decoration: none; color: inherit;" href="https://younes-dev-v9ap.onrender.com/">
+              <p class="signature">Younes Raymond</p>
+            </a>
+            <div class="button-container">
+                <a href="https://www.linkedin.com/in/younes-raymond-188a40241/" style="text-decoration: none;">
+                    <button class="button">Linkedin</button>
+                </a>
+            </div>
+            <p>message sent to ${companyEmail}: ${messageId}</p>
+            <img src="https://ajial.onrender.com/api/v1/track?id=${messageId}" width="1" height="1" style="display:none;">
+        </body>
+        </html>
+          `,
+          attachments: [
+            {
+              filename: 'cover_letter.txt',
+              content: 'Dear Hiring Manager, I am writing to express my interest in the open web developer position at your company. Please find my cover letter below for your consideration. My name is Younes Raymond, and I am a professional web developer with five years of experience in database administration and website design. I possess strong creative and analytical skills, making me a valuable asset to any development team. Skills: Front End Coding (React.js, Redux, Material-UI), Back End Development (Node.js, MongoDB), Computer Literacy (Bash scripting, Project Management Tools), Languages (French - Fluent, English - Proficient). Education: freeCodeCamp (2016 - 2017) - Front-End Engineering Certificate, codeCademy (2017 - 2019) - Full Stack Web Developer Certificate. Experience: Ajial-Amanegment Company (2018 - Present) - Applications Developer, pieceX Company (2018 - 2020) - Web Content Manager, Ajial-Amanagment Company (2019 - Current) - Analysis Content. I am confident in my ability to contribute effectively to your team and bring my technical expertise to your projects. I look forward to the opportunity to discuss how my skills align with the needs of your company. Thank you for considering my application. Best regards, Younes Raymond',
+            },
+            {
+              filename: 'resume.pdf',
+              path: './web developer -resume.pdf', // Replace with the actual path to your PDF file
+            },
+          ],
+        };
+
+        try {
+          const info = await transporter.sendMail(mailOptions);
+          console.log(`Message sent to ${companyEmail}: ${messageId}`);
+          emailLog.push({ company: companyEmail, success: true, messageId: messageId });
+
+          if (emailLog.length % 50 === 0) {
+            console.log('Pausing for 25 hours...');
+            await new Promise((resolve) => setTimeout(resolve, pauseDuration));
+          }
+        } catch (error) {
+          console.error(`Error sending email to ${companyEmail}:`, error);
+          let errorMessage;
+          if (error.code === 'EENVELOPE') {
+            errorMessage = 'Invalid recipient address';
+          } else {
+            errorMessage = error.message;
+          }
+          emailLog.push({ company: companyEmail, success: false, error: errorMessage });
+        }
+      }
+    }
+  }
+
+  const now = new Date();
+  const formattedDate = `${now.getMonth() + 1}-${now.getDate()}-${now.getFullYear()}`;
+const formattedTime = `${now.getHours()}_${now.getMinutes()}`;
+const logFileName = `./emailLog_${formattedDate}_${formattedTime}.json`;
+  fs.writeFileSync(logFileName, JSON.stringify(emailLog, null, 2));
+  console.log(`Email log saved to ${logFileName}`);
+};
+// sendEmail()
+
+
+
+// const messageId = generateUniqueId()
+// console.log(messageId)
