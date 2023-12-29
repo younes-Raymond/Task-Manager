4b2404f2bcc9be124395f6647d82a0892594d28d
younes1337 <raymondyounes2@gmail.com>
2023-12-29 19:13:53 +0100

    socket.io

--- taskmanagementproject/backend/server.js
+++ taskmanagementproject/backend/server.js
@@ -71,6 +71,6 @@ httpServer.listen(PORT, () => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
